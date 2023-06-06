eaf4ec0aaa61a8ebf1dd09a9667448922ac32170
washito0407 <washovilla78@gmail.com>
2023-06-06 07:31:55 -0800

    Proyecto geometria

--- src/figuras2d.java
+++ src/figuras2d.java
@@ -1,2 +1,19 @@
 public class figuras2d {
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+
+    public void setLado2(int lado2) {
+        this.lado2 = lado2;
+    }
+
+    public void setLado3(int lado3) {
+        this.lado3 = lado3;
+    }
+
+    int lado;
+    int lado2;
+    int lado3;
+
 }
